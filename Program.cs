58068164973e3c8b99c38af601240c0e99ecd70f
Jaxel Rojas Lopez <jrojaslopez@microsoft.com>
2022-08-25 20:00:56 -0400

    Repro request for otel geneva exporter

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using open_tel_geneva_exporter_repro;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
+
+var host = Host.CreateDefaultBuilder(args)
+     .ConfigureLogging((context, logginConfigBuilder) =>
+  {
+      logginConfigBuilder.ClearProviders();
+      logginConfigBuilder.AddConsole();
+
+      string? serviceName = typeof(Program).Assembly.GetName().Name;
+      string? version = typeof(Program).Assembly.GetName().Version?.ToString();
+      var resourceBuilder = ResourceBuilder.CreateDefault().AddService(serviceName: serviceName, serviceVersion: version);
+
+      logginConfigBuilder.AddOpenTelemetry(options =>
+      {
+          options.SetResourceBuilder(resourceBuilder);
+
+          options.AddGenevaLogExporter(exporterOptions =>
+          {
+              exporterOptions.ConnectionString = "EtwSession=OpenTelemetry";
+              exporterOptions.TableNameMappings = new Dictionary<string, string>()
+              {
+                  ["*"] = "CategoryEvent"
+              };
+          });
+
+          options.AddConsoleExporter();
+
+          options.IncludeFormattedMessage = false;
+          options.ParseStateValues = true;
+          options.IncludeScopes = true;
+      });
+  });
+
+var app = host.Build();
+
+var logger = app.Services.GetService<ILogger<Program>>();
+
+app.RunAsync();
+
+logger.ConsoleStarted(nameof(Program));
+logger.ConsoleExited(nameof(Program));
---
+++ SampleLogs.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace open_tel_geneva_exporter_repro;
+
+public static partial class SampleLogs
+{
+    [LoggerMessage
+    (EventId = (int)LogEvents.ConsoleStarted,
+     EventName = nameof(LogEvents.ConsoleStarted),
+     Level = LogLevel.Information,
+     Message = "{consoleName} Console started")]
+    public static partial void ConsoleStarted(this ILogger logger, string? consoleName);
+
+    [LoggerMessage
+        (EventId = (int)LogEvents.ConsoleExited,
+        EventName = nameof(LogEvents.ConsoleExited),
+        Level = LogLevel.Information,
+        Message = "{consoleName} Console exited")]
+    public static partial void ConsoleExited(this ILogger logger, string? consoleName);
+}
+
+internal enum LogEvents
+{
+    ConsoleStarted = 1,
+    ConsoleExited = 2
+}
